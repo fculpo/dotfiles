9779579ab85539c5fd91669bcbd593ff4c519b28
Fabien Culpo <fabien.culpo@gmail.com>
2025-11-16 13:28:56 +0100

    now thats a zshrc and a nice zimrc

--- dot_zsh.d/executable_aliases.zsh
+++ dot_zsh.d/executable_aliases.zsh
@@ -3,9 +3,7 @@ alias crypto='curl rate.sx' # Get crypto prices
 # wget sucks with certificates. Let's keep it simple.
 alias wget="curl -O"
 
-[[ "$(command -v colordiff)" ]] && alias diff='colordiff'
-
-alias mount='mount |column -t'
+# [[ "$(command -v colordiff)" ]] && alias diff='colordiff'
 
 alias h='history'
 alias j='jobs -l'
@@ -15,15 +13,16 @@ alias nowtime=now
 alias nowdate='date +"%d-%m-%Y"'
 alias ports='netstat -tulanp'
 
-# confirmation #
+# confirmation before overwriting files
 alias mv='mv -i'
 alias cp='cp -i'
 alias ln='ln -i'
 
-#progress bar on file copy. Useful evenlocal.
+# Progress bar on file copy. Useful evenlocal.
 alias cpProgress="rsync --progress -ravz"
 
 alias tf="terraform"
+alias tg="terragrunt"
 
 alias g="git"
 alias gs="git status -sb"
@@ -33,7 +32,7 @@ alias vi="vim"
 alias kx="kubectx"
 alias kn="kubens"
 
-alias r="source ~/.zshrc"
+alias r="exec zsh"
 
 alias myip='curl http://ipecho.net/plain; echo'
 
@@ -43,135 +42,14 @@ if [[ -f "/System/Library/CoreServices/ScreenSaverEngine.app/Contents/MacOS/Scre
 fi
 
 [[ "$(command -v prettyping)" ]] && alias ping="prettyping --nolegend"
-
-[[ "$(command -v bat)" ]] && alias cat="bat"
-
+[[ "$(command -v prettyping)" ]] && alias ping6="prettyping --nolegend --ipv6"
 [[ "$(command -v htop)" ]] && alias top="htop"
-
-if hash eza 2>/dev/null; then
-  alias ls='eza'                                                          # ls
-  alias l='eza -lbF --git'                                                # list, size, type, git
-  alias ll='eza -lbGF --git'                                             # long list
-  alias llm='eza -lbGd --git --sort=modified'                            # long list, modified date sort
-  alias la='eza -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-  alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-  alias lS='eza -1'                                                              # one column, just names
-  alias lt='eza --tree --level=2'                                         # tree
-fi
-
-if exa --icons &>/dev/null; then
-    alias ls='exa --git --icons'                             # system: List filenames on one line
-    alias l='exa --git --icons -lF'                          # system: List filenames with long format
-    alias ll='exa -lahF --git'                               # system: List all files
-    alias lll="exa -1F --git --icons"                        # system: List files with one line per file
-    alias llm='ll --sort=modified'                           # system: List files by last modified date
-    alias la='exa -lbhHigUmuSa --color-scale --git --icons'  # system: List files with attributes
-    alias lx='exa -lbhHigUmuSa@ --color-scale --git --icons' # system: List files with extended attributes
-    alias lt='exa --tree --level=2'                          # system: List files in a tree view
-    alias llt='exa -lahF --tree --level=2'                   # system: List files in a tree view with long format
-    alias ltt='exa -lahF | grep "$(date +"%d %b")"'          # system: List files modified today
-elif command -v exa &>/dev/null; then
-    alias ls='exa --git'
-    alias l='exa --git -lF'
-    alias ll='exa -lahF --git'
-    alias lll="exa -1F --git"
-    alias llm='ll --sort=modified'
-    alias la='exa -lbhHigUmuSa --color-scale --git'
-    alias lx='exa -lbhHigUmuSa@ --color-scale --git'
-    alias lt='exa --tree --level=2'
-    alias llt='exa -lahF --tree --level=2'
-    alias ltt='exa -lahF | grep "$(date +"%d %b")"'
-elif command -v colorls &>/dev/null; then
-    alias ll="colorls -1A --git-status"
-    alias ls="colorls -A"
-    alias ltt='colorls -A | grep "$(date +"%d %b")"'
-elif [[ $(command -v ls) =~ gnubin || $OSTYPE =~ linux ]]; then
-    alias ls="ls --color=auto"
-    alias ll='ls -FlAhpv --color=auto'
-    alias ltt='ls -FlAhpv| grep "$(date +"%d %b")"'
-else
-    alias ls="ls -G"
-    alias ll='ls -FGlAhpv'
-    alias ltt='ls -FlAhpv| grep "$(date +"%d %b")"'
-fi
 
 mcd() {
     # DESC: Create a directory and enter it
     # USAGE: mcd [dirname]
     mkdir -pv "$1"
     cd "$1" || exit
-}
-
-extract() {
-    # DESC:  Extracts a compressed file from multiple formats
-    # USAGE: extract -v <file>
-
-    local opt
-    local OPTIND=1
-
-    while getopts "hv" opt; do
-        case "$opt" in
-            h)
-                cat <<EOF
-  $ ${FUNCNAME[0]} [option] <archives>
-  options:
-    -h  show this message and exit
-    -v  verbosely list files processed
-EOF
-                return
-                ;;
-            v)
-                local -r verbose='v'
-                ;;
-            ?)
-                extract -h >&2
-                return 1
-                ;;
-        esac
-    done
-    shift $((OPTIND - 1))
-
-    [ $# -eq 0 ] && extract -h && return 1
-    while [ $# -gt 0 ]; do
-        if [ -f "$1" ]; then
-            case "$1" in
-                *.tar.bz2 | *.tbz | *.tbz2) tar "x${verbose}jf" "$1" ;;
-                *.tar.gz | *.tgz) tar "x${verbose}zf" "$1" ;;
-                *.tar.xz)
-                    xz --decompress "$1"
-                    set -- "$@" "${1:0:-3}"
-                    ;;
-                *.tar.Z)
-                    uncompress "$1"
-                    set -- "$@" "${1:0:-2}"
-                    ;;
-                *.bz2) bunzip2 "$1" ;;
-                *.deb) dpkg-deb -x${verbose} "$1" "${1:0:-4}" ;;
-                *.pax.gz)
-                    gunzip "$1"
-                    set -- "$@" "${1:0:-3}"
-                    ;;
-                *.gz) gunzip "$1" ;;
-                *.pax) pax -r -f "$1" ;;
-                *.pkg) pkgutil --expand "$1" "${1:0:-4}" ;;
-                *.rar) unrar x "$1" ;;
-                *.rpm) rpm2cpio "$1" | cpio -idm${verbose} ;;
-                *.tar) tar "x${verbose}f" "$1" ;;
-                *.txz)
-                    mv "$1" "${1:0:-4}.tar.xz"
-                    set -- "$@" "${1:0:-4}.tar.xz"
-                    ;;
-                *.xz) xz --decompress "$1" ;;
-                *.zip | *.war | *.jar) unzip "$1" ;;
-                *.Z) uncompress "$1" ;;
-                *.7z) 7za x "$1" ;;
-                *) echo "'$1' cannot be extracted via extract" >&2 ;;
-            esac
-        else
-            echo "extract: '$1' is not a valid file" >&2
-        fi
-        shift
-    done
 }
 
 j2y() {
@@ -230,8 +108,9 @@ if [[ ${OSTYPE} == "darwin"* ]]; then # Only load these on a MacOS computer
         qlmanage -p "${*}" &>/dev/null
     }
 
-    alias cleanupLS="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder" # Clean up LaunchServices to remove duplicates in the "Open With" menu
-
+    # Clean up LaunchServices to remove duplicates in the "Open With" menu
+    alias cleanupLS="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder" 
+    
     unquarantine() {
         # DESC:  Manually remove a downloaded app or file from the MacOS quarantine
         # ARGS:  $1 (required): Path to file or app
